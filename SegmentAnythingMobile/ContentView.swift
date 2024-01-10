0ca70edfb497736e21900697fcd97f69e50f0a49
Alessandro Toschi <aletos@plexagon.it>
2024-01-10 11:55:37 +0100

    Initial Commit

---
+++ SegmentAnythingMobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SegmentAnythingMobile
+//
+//  Created by Alessandro Toschi on 10/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SegmentAnythingMobile/SegmentAnythingMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentAnythingMobileApp.swift
+//  SegmentAnythingMobile
+//
+//  Created by Alessandro Toschi on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SegmentAnythingMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
